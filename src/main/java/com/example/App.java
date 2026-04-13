9c1b5d163c0513cadc904137ce2acf0b0e36b681
Hruday P N <hrudaypn2005@gmail.com>
2026-04-13 08:56:20 +0530

    Configure ChromeOptions for headless browser

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,21 +3,26 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        WebDriver driver=new ChromeDriver();
+public class App {
+    public static void main(String[] args) {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--headless=new");   // 
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
+        driver.quit();
     }
 }
-
